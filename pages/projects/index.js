a28b83e931c3c7d92c17e18698a3c21bd0b5cf8c
Sofia Darke <sofia.darke@hyperisland.se>
2021-09-10 11:25:27 +0200

    test without masonry

--- pages/projects/index.js
+++ pages/projects/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Image from "next/image";
 import Head from "next/head";
-import Masonry from "react-masonry-css";
+// import Masonry from "react-masonry-css";
 
 const projects = () => {
 	const breakpointColumnsObj = {
@@ -16,47 +16,47 @@ const projects = () => {
 				<title> Moa Hamner | Projects</title>
 				<meta name="keywords" content="Moa Hamner - projects" />
 			</Head>
-			<Masonry
+			{/* <Masonry
 				breakpointCols={breakpointColumnsObj}
 				className="my-masonry-grid"
 				columnClassName="my-masonry-grid_column"
-			>
-				<div className="img-in-grid">
-					<div className="project-text-box">
-						<p className="project-title">Burkar och tanter</p>
-						<p className="project-sub-title">Sound 2019</p>
-					</div>
-					<Image src="/burkar.jpeg" width={1000} height={563} />
+			> */}
+			<div className="img-in-grid">
+				<div className="project-text-box">
+					<p className="project-title">Burkar och tanter</p>
+					<p className="project-sub-title">Sound 2019</p>
 				</div>
-				<div className="img-in-grid">
-					<div className="project-text-box">
-						<p className="project-title">Burkar och tanter</p>
-						<p className="project-sub-title">Sound 2019</p>
-					</div>
-					<Image src="/project6.png" width={3354} height={2046} />
+				<Image src="/burkar.jpeg" width={1000} height={563} />
+			</div>
+			<div className="img-in-grid">
+				<div className="project-text-box">
+					<p className="project-title">Burkar och tanter</p>
+					<p className="project-sub-title">Sound 2019</p>
 				</div>
-				<div className="img-in-grid">
-					<Image src="/soritoni.jpeg" width={1280} height={720} />
-				</div>
-				<div className="img-in-grid">
-					<Image src="/undersokningen.jpeg" width={560} height={365} />
-				</div>
-				<div className="img-in-grid">
-					<Image src="/uppdrag_gamling.jpg" width={774} height={629} />
-				</div>
-				<div className="img-in-grid">
-					<Image src="/project8.png" width={3580} height={1908} />
-				</div>
-				<div className="img-in-grid">
-					<Image src="/burkar.jpeg" width={1000} height={563} />
-				</div>
-				<div className="img-in-grid">
-					<Image src="/project8.png" width={3580} height={1908} />
-				</div>
-				<div className="img-in-grid">
-					<Image src="/soritoni.jpeg" width={1280} height={720} />
-				</div>
-			</Masonry>
+				<Image src="/project6.png" width={3354} height={2046} />
+			</div>
+			<div className="img-in-grid">
+				<Image src="/soritoni.jpeg" width={1280} height={720} />
+			</div>
+			<div className="img-in-grid">
+				<Image src="/undersokningen.jpeg" width={560} height={365} />
+			</div>
+			<div className="img-in-grid">
+				<Image src="/uppdrag_gamling.jpg" width={774} height={629} />
+			</div>
+			<div className="img-in-grid">
+				<Image src="/project8.png" width={3580} height={1908} />
+			</div>
+			<div className="img-in-grid">
+				<Image src="/burkar.jpeg" width={1000} height={563} />
+			</div>
+			<div className="img-in-grid">
+				<Image src="/project8.png" width={3580} height={1908} />
+			</div>
+			<div className="img-in-grid">
+				<Image src="/soritoni.jpeg" width={1280} height={720} />
+			</div>
+			{/* </Masonry> */}
 			{/* 
 			<div className="project-grid">
 				<div className="img-in-grid">
